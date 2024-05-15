882e626a9f12dcf5c4789b84e136994729a025db
Ramlu <39337840+Ramlu@users.noreply.github.com>
2024-05-15 20:01:36 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "nav-terris-datastore-demo-1" # Replace with your actual S3 bucket name
+    bucket = "nav-terris-datastore-demo-2" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
